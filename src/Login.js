7a5a03ad8de2d2d20dca96f260d4230525bb09cd
Dan Suceava <dan.suceava@gmail.com>
2021-12-09 13:44:21 -0700

    add aws-amplify package

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,18 @@
 import { API } from 'aws-amplify';
 import React from 'react';
 
+import awsconfig from './aws-exports';
+
 function Login(props) {
   const { setToken } = props;
+  console.log(awsconfig);
 
+  const loginUrl = `${awsconfig.aws_cloud_logic_custom[0].endpoint}/auth/login`;
 
   return (
     <div className="App">
       <header className="App-header">
-        <a className="btn-spotify" href="/auth/login" >
+        <a className="btn-spotify" href={loginUrl} >
           Login with Spotify 
         </a>
       </header>
