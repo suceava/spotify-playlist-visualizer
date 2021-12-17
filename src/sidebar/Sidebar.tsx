4ef17c99a35e965f0e8ac497018921d9ecefd793
Dan Suceava <dan.suceava@gmail.com>
2021-12-17 16:19:21 -0700

    add list of playlist analyses

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -1,23 +1,53 @@
+import List from "@mui/material/List";
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { useCallback, useState } from "react";
+
+import { PlaylistData } from '../data/playlistData';
+
 import './sidebar.css';
 
 export interface SidebarProps {
-  currentPlaylist: any;
+  currentPlaylist: SpotifyApi.SinglePlaylistResponse | null;
   playlistMap: Map<string, any>;
+  playlistData: PlaylistData[];
 }
 
-export function Sidebar({ currentPlaylist, playlistMap }: SidebarProps) {
-
+export function Sidebar({ currentPlaylist, playlistMap, playlistData }: SidebarProps) {
+  const [selectedPlaylist, setSelectedPlaylist] = useState<string | null>(null);
+  const onListItemClick = useCallback(
+    (event, playlist, index) => {
+      console.log(playlist, index);
+      setSelectedPlaylist(playlist.name);
+    },
+    [playlistData],
+  )
   return (
     <div className="sidebar">
-      {currentPlaylist && 
-        <div className="sidebar-playlist-info">
-          <img src={currentPlaylist.images[0].url} alt="" />
-          <div className="sidebar-playlist-info-text">
-            <h2>{currentPlaylist.name}</h2>
-            <p>{`${currentPlaylist.tracks.items.length} tracks`}</p>
+      <div className="sidebar-playlist">
+        { currentPlaylist && 
+          <div className="sidebar-playlist-info">
+            <img src={currentPlaylist.images[0].url} alt="" />
+            <div className="sidebar-playlist-info-text">
+              <h2>{currentPlaylist.name}</h2>
+              <p>{`${currentPlaylist.tracks.items.length} tracks`}</p>
+            </div>
           </div>
-        </div>
-      }
+        }
+      </div>
+      <hr/>
+      <div className="sidebar-playlist-data">
+        <List dense={true}>
+          {
+            playlistData.map((playlist, i) => (
+              <ListItemButton key={i} onClick={(e) => onListItemClick(e, playlist, i)} selected={selectedPlaylist === playlist.name}>
+                <ListItemText>{playlist.name}</ListItemText>
+              </ListItemButton>
+            ))
+          }
+        </List>
+      </div>
     </div>
   );
 }
